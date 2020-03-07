d8fef505e618ce0408d76d0d0c5c7512bc9ac82f
rheaacharya77 <ria.acharya07@gmail.com>
2020-03-07 12:25:34 +0545

    schemas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var fellowdetailsSchema= require('../models/fellow');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/addfellow', function (req, res, next) {
+  //Movies.find().exec(function(err,movies){
+  res.render('addfellow')
+  //})
+
+});
+
+router.post('/addfellow', function (req, res, next) {
+  console.log(req.body)
+  var fellow = new fellow({
+    name: req.body.name,
+    batch: req.body.batch,
+    college: req.body.college,
+
+  })
+  var promise = fellow.save()
+  promise.then((fellow) => {
+    console.log('fellow saved', fellow)
+
+  })
+  res.redirect('/home');
+
 });
 
 module.exports = router;
